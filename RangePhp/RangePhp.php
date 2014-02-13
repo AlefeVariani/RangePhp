95cc8b8d0dbb62090c7e5d7c80229633556a1ef9
Alefe Variani <alefevariani@unochapeco.edu.br>
2014-02-13 10:45:13 -0200

    rangePHP

---
+++ RangePhp/RangePhp.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @author: Alefe Variani <alefevariani@unochapeco.edu.br>
+* Utilifando Função "range" do PHP, para mostras alfabeto e sequencia de numeros 
+*
+*
+* range(valorMinimo, valorMaximo, Parametro)
+*
+* Mais informações: http://www.php.net/manual/pt_BR/function.range.php
+*/
+
+//Mostrando Alfabeto
+foreach (range('A', 'Z') as $letra){
+	print $letra;
+	echo "\n";
+}
+
+//Usado para numeros
+foreach (range(0, 100) as $numero){
+	print $numero;
+	echo "\n";
+}
+
